16da4144a34e610b273ff69ec7252cc7202bca1d
Juliano Batistim <jubatistim@gmail.com>
2020-07-20 22:16:04 -0300

    added 2 outputs with probability of each class

--- cnn-neural-network.py
+++ cnn-neural-network.py
@@ -50,15 +50,20 @@ cnn.add(keras.layers.MaxPool2D(pool_size=2, strides=2, padding='valid'))
 cnn.add(keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu"))
 cnn.add(keras.layers.MaxPool2D(pool_size=2, strides=2, padding='valid'))
 
+# cnn.add(keras.layers.Dropout(0.25))
+
 ### Step 3 - Flattening
 cnn.add(keras.layers.Flatten())
+
+# cnn.add(keras.layers.Dropout(0.5))
 
 ### Step 4 - Full Connection
 cnn.add(keras.layers.Dense(units=128, activation='relu'))
 
+# cnn.add(keras.layers.Dropout(0.5))
+
 ### Step 5 - Output Layer
-cnn.add(keras.layers.Dense(units=1, activation='sigmoid')) # DEFAULT
-# cnn.add(keras.layers.Dense(units=2, activation='softmax')) # TRYING TO FIND %, outputs between 0 and 1
+cnn.add(keras.layers.Dense(units=2, activation='softmax')) # find probability of each class, outputs between 0 and 1 for each class that represents the probability
 
 ### Summarize the network
 cnn.summary()
@@ -68,21 +73,7 @@ cnn.summary()
 ###############################
 
 ### Compiling the CNN
-cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy']) # DEFAULT
-
-# SIGMOID ACTIVIATION
-# cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy']) # Seems the best one, but always output 0 or 1
-# cnn.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy']) # DON'T WORK
-# cnn.compile(optimizer = 'adam', loss = 'poisson', metrics = ['accuracy']) # Seems work, but always output 0 or 1
-# cnn.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['accuracy']) # Seems work, but always output 0 or 1
-
-# SOFTMAX ACTIVATION
-# cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy']) # strange results with same loss value during several epochs. THE MODEL FOR PREDICTIONS WITH 10 EPOCHS IS VERY BAD
-# cnn.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy']) # accuracy and loss seems good during the trainning, but it seems to always output 0 or 1. ACTUALLY THIS IS THE GOOD MODEL FOR PREDICTIONS.
-# cnn.compile(optimizer = 'adam', loss = 'poisson', metrics = ['accuracy']) # strange results with same loss value during several epochs. THE MODEL FOR PREDICTIONS WITH 10 EPOCHS IS VERY BAD
-# cnn.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['accuracy']) # strange results with same loss value during several epochs. THE MODEL FOR PREDICTIONS WITH 10 EPOCHS IS VERY BAD
-
-
+cnn.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
 
 
 ### Training the CNN on the Training set and evaluating it on the Test set
